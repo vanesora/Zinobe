f958f9b3ca45120f169e3ed0898605d477e470b2
Vane <primestone\vanessa.sora.ext>
2020-06-15 17:35:57 -0500

    menu list / style

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  public menuItems = [
+    { name: 'Nuevo prestamo', select: true, icon: 'add_circle_outline', path: 'new-overdraft' },
+    { name: 'Nuevo usuario', select: false, icon: 'person_add_alt_1', path: 'new-user' },
+    { name: 'Usuarios', select: false, icon: 'account_circle', path: 'users' },
+    { name: 'Prestamos aprovados', select: false, icon: 'check_circle', path: 'approved' },
+    { name: 'Prestamos rechazados', select: false, icon: 'cancel', path: 'rejected' },
+  ];
+
+  constructor(
+    // tslint:disable-next-line: variable-name
+    private _router: Router,
+  ) { }
 
   ngOnInit() {
+  }
+
+  public selectMenuItem(item): void {
+    this.menuItems = this.menuItems.map((itemMenu) => {
+      itemMenu.select = false;
+      return itemMenu;
+    });
+    item.select = true;
+    this._router.navigate([`/home/${item.path}`]);
   }
 
 }
