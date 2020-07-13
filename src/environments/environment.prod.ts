fe483adf979223ddad7ec412acb32a9a9b9a95a8
Vane <primestone\vanessa.sora.ext>
2020-07-13 14:57:28 -0500

    enviroments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,4 @@
 export const environment = {
   production: true,
   capitalBase: 10000000,
-  firebase : {
-    apiKey: "AIzaSyBaLJtqI6vMFBsiBbFzGN1f3GJ0pnMFGV4",
-    authDomain: "zinobe-47d14.firebaseapp.com",
-    databaseURL: "https://zinobe-47d14.firebaseio.com",
-    projectId: "zinobe-47d14",
-    storageBucket: "zinobe-47d14.appspot.com",
-    messagingSenderId: "440799601098",
-    appId: "1:440799601098:web:641cf48cf68664e1c5af33"
-  }
 };
